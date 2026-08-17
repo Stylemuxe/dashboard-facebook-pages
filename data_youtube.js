1bc28a2afb2b825191d838bb58ef8aba5bd8e254
Irving Fabian <stylemuxe@gmail.com>
2026-08-17 14:25:21 -0600

    Actualizacion automatica de YouTube 2026-08-17 14:25

---
+++ data_youtube.js
@@ -0,0 +1,223 @@
+window.YOUTUBE_DATA = {
+ "generado": "2026-08-17 14:25",
+ "canales": {
+  "equilibrio_total": {
+   "nombre": "Equilibrio Total MX",
+   "handle": "EquilibrioTotalmx",
+   "channel_id": "UCn6XCXHFXnr5-UqiH4s9QMA",
+   "thumbnail": "https://yt3.ggpht.com/29LSxcHJOorUyrR0Aoz5TW4C_sUrwI6iLarJTG_f2pzFMhl2mrBxlhw91EY-MYKP0n5yi1Ip=s240-c-k-c0x00ffffff-no-rj",
+   "seguidores": 7180,
+   "vistas_totales": 1564611,
+   "video_count": 88,
+   "historial_seguidores": [
+    {
+     "fecha": "2026-08-17",
+     "seguidores": 7180,
+     "vistas_totales": 1564611
+    }
+   ],
+   "videos": [
+    {
+     "id": "Gz1N9w7lcAI",
+     "titulo": "¿Por quién vienes?",
+     "fecha": "2026-07-24T06:00:06Z",
+     "miniatura": "https://i.ytimg.com/vi/Gz1N9w7lcAI/mqdefault.jpg",
+     "url": "https://www.youtube.com/watch?v=Gz1N9w7lcAI",
+     "vistas": 267,
+     "likes": 2,
+     "comentarios": 0
+    },
+    {
+     "id": "f6euhUlrGE8",
+     "titulo": "2 Ejercicios Clave para Moverte sin Dolor 💪",
+     "fecha": "2026-07-11T15:51:28Z",
+     "miniatura": "https://i.ytimg.com/vi/f6euhUlrGE8/mqdefault.jpg",
+     "url": "https://www.youtube.com/watch?v=f6euhUlrGE8",
+     "vistas": 1703,
+     "likes": 25,
+     "comentarios": 0
+    },
+    {
+     "id": "koNKV9O2q_M",
+     "titulo": "Clase de Zumba para Adultos Mayores",
+     "fecha": "2026-06-10T16:00:09Z",
+     "miniatura": "https://i.ytimg.com/vi/koNKV9O2q_M/mqdefault.jpg",
+     "url": "https://www.youtube.com/watch?v=koNKV9O2q_M",
+     "vistas": 374,
+     "likes": 7,
+     "comentarios": 0
+    },
+    {
+     "id": "MCcWxjoepeI",
+     "titulo": "No necesitas experiencia, solo ganas de moverte…🥰💃✨ No te pierdas nuestra clase completa.",
+     "fecha": "2026-06-08T23:09:35Z",
+     "miniatura": "https://i.ytimg.com/vi/MCcWxjoepeI/mqdefault.jpg",
+     "url": "https://www.youtube.com/watch?v=MCcWxjoepeI",
+     "vistas": 927,
+     "likes": 13,
+     "comentarios": 6
+    },
+    {
+     "id": "Xufj_Xp8liI",
+     "titulo": "Adiós al dolor con Acupuntura Clínica 💉✨",
+     "fecha": "2026-04-30T21:45:02Z",
+     "miniatura": "https://i.ytimg.com/vi/Xufj_Xp8liI/mqdefault.jpg",
+     "url": "https://www.youtube.com/watch?v=Xufj_Xp8liI",
+     "vistas": 276,
+     "likes": 8,
+     "comentarios": 0
+    },
+    {
+     "id": "OhKv1VJzarI",
+     "titulo": "Recuperar tu movilidad sin darte cuenta ✨",
+     "fecha": "2026-04-29T21:45:02Z",
+     "miniatura": "https://i.ytimg.com/vi/OhKv1VJzarI/mqdefault.jpg",
+     "url": "https://www.youtube.com/watch?v=OhKv1VJzarI",
+     "vistas": 226,
+     "likes": 4,
+     "comentarios": 0
+    },
+    {
+     "id": "qTNYZDArfHI",
+     "titulo": "De una fractura de fémur a caminar de nuevo 🏃‍♂️✨",
+     "fecha": "2026-04-28T19:45:05Z",
+     "miniatura": "https://i.ytimg.com/vi/qTNYZDArfHI/mqdefault.jpg",
+     "url": "https://www.youtube.com/watch?v=qTNYZDArfHI",
+     "vistas": 261,
+     "likes": 4,
+     "comentarios": 0
+    },
+    {
+     "id": "LMC_tk0I2fM",
+     "titulo": "¿Operación? Mira esto antes 😱",
+     "fecha": "2026-04-27T21:00:21Z",
+     "miniatura": "https://i.ytimg.com/vi/LMC_tk0I2fM/mqdefault.jpg",
+     "url": "https://www.youtube.com/watch?v=LMC_tk0I2fM",
+     "vistas": 220,
+     "likes": 1,
+     "comentarios": 0
+    },
+    {
+     "id": "L6z50ET7Bp0",
+     "titulo": "Vivir con dolor NO es normal 🚫",
+     "fecha": "2026-04-26T23:00:15Z",
+     "miniatura": "https://i.ytimg.com/vi/L6z50ET7Bp0/mqdefault.jpg",
+     "url": "https://www.youtube.com/watch?v=L6z50ET7Bp0",
+     "vistas": 311,
+     "likes": 5,
+     "comentarios": 0
+    },
+    {
+     "id": "I5bVg8KWBMY",
+     "titulo": "Manguito rotador | Lesión y tratamiento integral",
+     "fecha": "2026-03-16T21:00:19Z",
+     "miniatura": "https://i.ytimg.com/vi/I5bVg8KWBMY/mqdefault.jpg",
+     "url": "https://www.youtube.com/watch?v=I5bVg8KWBMY",
+     "vistas": 1536,
+     "likes": 37,
+     "comentarios": 0
+    },
+    {
+     "id": "aNONVOmvrOY",
+     "titulo": "¿Por Qué Te Duele? 3 Especialistas Explican 4 Lesiones Comunes | Equilibrio al Aire",
+     "fecha": "2026-03-15T23:00:06Z",
+     "miniatura": "https://i.ytimg.com/vi/aNONVOmvrOY/mqdefault.jpg",
+     "url": "https://www.youtube.com/watch?v=aNONVOmvrOY",
+     "vistas": 428,
+     "likes": 8,
+     "comentarios": 4
+    },
+    {
+     "id": "y-4nAByhwTs",
+     "titulo": "Tu cuerpo te lo agradecerá ✨☺️",
+     "fecha": "2026-03-15T21:00:55Z",
+     "miniatura": "https://i.ytimg.com/vi/y-4nAByhwTs/mqdefault.jpg",
+     "url": "https://www.youtube.com/watch?v=y-4nAByhwTs",
+     "vistas": 1708,
+     "likes": 33,
+     "comentarios": 0
+    },
+    {
+     "id": "me7pMyCBvTY",
+     "titulo": "“No podía destapar el café” ☕️",
+     "fecha": "2026-03-13T21:01:02Z",
+     "miniatura": "https://i.ytimg.com/vi/me7pMyCBvTY/mqdefault.jpg",
+     "url": "https://www.youtube.com/watch?v=me7pMyCBvTY",
+     "vistas": 663,
+     "likes": 13,
+     "comentarios": 0
+    },
+    {
+     "id": "xS_UjzGvw8A",
+     "titulo": "¿Vives con dolor?🤕",
+     "fecha": "2026-03-11T21:01:07Z",
+     "miniatura": "https://i.ytimg.com/vi/xS_UjzGvw8A/mqdefault.jpg",
+     "url": "https://www.youtube.com/watch?v=xS_UjzGvw8A",
+     "vistas": 799,
+     "likes": 19,
+     "comentarios": 1
+    },
+    {
+     "id": "BxIz9LGdZ2o",
+     "titulo": "Rutina de Ejercicios para Dolor de Hombro | Mejora Movilidad y Disminuye la Molestia",
+     "fecha": "2026-02-27T22:51:07Z",
+     "miniatura": "https://i.ytimg.com/vi/BxIz9LGdZ2o/mqdefault.jpg",
+     "url": "https://www.youtube.com/watch?v=BxIz9LGdZ2o",
+     "vistas": 248,
+     "likes": 5,
+     "comentarios": 0
+    },
+    {
+     "id": "v6LRCc5K8CU",
+     "titulo": "Conoce a Daniel “El Dragón” Sánchez 🥊🔥",
+     "fecha": "2026-02-23T23:00:21Z",
+     "miniatura": "https://i.ytimg.com/vi/v6LRCc5K8CU/mqdefault.jpg",
+     "url": "https://www.youtube.com/watch?v=v6LRCc5K8CU",
+     "vistas": 258,
+     "likes": 5,
+     "comentarios": 2
+    },
+    {
+     "id": "9h5c3RFr7PI",
+     "titulo": "Conoce a Daniel “El Dragón” Sánchez 🥊🔥",
+     "fecha": "2026-02-23T21:00:10Z",
+     "miniatura": "https://i.ytimg.com/vi/9h5c3RFr7PI/mqdefault.jpg",
+     "url": "https://www.youtube.com/watch?v=9h5c3RFr7PI",
+     "vistas": 1793,
+     "likes": 32,
+     "comentarios": 0
+    },
+    {
+     "id": "wrZhqTW0lV8",
+     "titulo": "Rutina completa de calistenia para principiantes",
+     "fecha": "2025-12-27T18:43:04Z",
+     "miniatura": "https://i.ytimg.com/vi/wrZhqTW0lV8/mqdefault.jpg",
+     "url": "https://www.youtube.com/watch?v=wrZhqTW0lV8",
+     "vistas": 851,
+     "likes": 22,
+     "comentarios": 3
+    },
+    {
+     "id": "TW0OudIhLek",
+     "titulo": "¿Qué pasa con tu cuerpo durante el invierno?",
+     "fecha": "2025-12-04T18:35:58Z",
+     "miniatura": "https://i.ytimg.com/vi/TW0OudIhLek/mqdefault.jpg",
+     "url": "https://www.youtube.com/watch?v=TW0OudIhLek",
+     "vistas": 788,
+     "likes": 21,
+     "comentarios": 0
+    },
+    {
+     "id": "ZLHIl1gTAI4",
+     "titulo": "Fisioterapia para adultos mayores",
+     "fecha": "2025-11-26T23:31:58Z",
+     "miniatura": "https://i.ytimg.com/vi/ZLHIl1gTAI4/mqdefault.jpg",
+     "url": "https://www.youtube.com/watch?v=ZLHIl1gTAI4",
+     "vistas": 1105,
+     "likes": 34,
+     "comentarios": 0
+    }
+   ]
+  }
+ }
+};
