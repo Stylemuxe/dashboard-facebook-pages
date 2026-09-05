6d61abe08bc583cd85105db49fc8c3f5d861431a
Irving Fabian <stylemuxe@gmail.com>
2026-09-05 10:09:14 -0600

    Actualizacion automatica de YouTube 2026-09-05 10:09

--- data_youtube.js
+++ data_youtube.js
@@ -1,5 +1,5 @@
 window.YOUTUBE_DATA = {
- "generado": "2026-09-05 09:20",
+ "generado": "2026-09-05 10:09",
  "canales": {
   "equilibrio_total": {
    "nombre": "Equilibrio Total MX",
@@ -7,7 +7,7 @@ window.YOUTUBE_DATA = {
    "channel_id": "UCn6XCXHFXnr5-UqiH4s9QMA",
    "thumbnail": "https://yt3.ggpht.com/29LSxcHJOorUyrR0Aoz5TW4C_sUrwI6iLarJTG_f2pzFMhl2mrBxlhw91EY-MYKP0n5yi1Ip=s240-c-k-c0x00ffffff-no-rj",
    "seguidores": 7460,
-   "vistas_totales": 1636745,
+   "vistas_totales": 1639277,
    "video_count": 88,
    "historial_seguidores": [
     {
@@ -108,7 +108,7 @@ window.YOUTUBE_DATA = {
     {
      "fecha": "2026-09-05",
      "seguidores": 7460,
-     "vistas_totales": 1636745
+     "vistas_totales": 1639277
     }
    ],
    "videos": [
@@ -188,7 +188,7 @@ window.YOUTUBE_DATA = {
      "fecha": "2026-04-27T21:00:21Z",
      "miniatura": "https://i.ytimg.com/vi/LMC_tk0I2fM/mqdefault.jpg",
      "url": "https://www.youtube.com/watch?v=LMC_tk0I2fM",
-     "vistas": 250,
+     "vistas": 251,
      "likes": 2,
      "comentarios": 0
     },
@@ -198,7 +198,7 @@ window.YOUTUBE_DATA = {
      "fecha": "2026-04-26T23:00:15Z",
      "miniatura": "https://i.ytimg.com/vi/L6z50ET7Bp0/mqdefault.jpg",
      "url": "https://www.youtube.com/watch?v=L6z50ET7Bp0",
-     "vistas": 348,
+     "vistas": 349,
      "likes": 5,
      "comentarios": 0
     },
@@ -208,7 +208,7 @@ window.YOUTUBE_DATA = {
      "fecha": "2026-03-16T21:00:19Z",
      "miniatura": "https://i.ytimg.com/vi/I5bVg8KWBMY/mqdefault.jpg",
      "url": "https://www.youtube.com/watch?v=I5bVg8KWBMY",
-     "vistas": 1571,
+     "vistas": 1572,
      "likes": 38,
      "comentarios": 0
     },
