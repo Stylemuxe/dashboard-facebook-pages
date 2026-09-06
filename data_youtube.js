1ade2f7b03b331076b95a239a80f5337c14cd6dd
Irving Fabian <stylemuxe@gmail.com>
2026-09-05 21:04:39 -0600

    Actualizacion automatica de YouTube 2026-09-05 21:04

--- data_youtube.js
+++ data_youtube.js
@@ -1,5 +1,5 @@
 window.YOUTUBE_DATA = {
- "generado": "2026-09-05 20:04",
+ "generado": "2026-09-05 21:04",
  "canales": {
   "equilibrio_total": {
    "nombre": "Equilibrio Total MX",
@@ -138,7 +138,7 @@ window.YOUTUBE_DATA = {
      "fecha": "2026-06-10T16:00:09Z",
      "miniatura": "https://i.ytimg.com/vi/koNKV9O2q_M/mqdefault.jpg",
      "url": "https://www.youtube.com/watch?v=koNKV9O2q_M",
-     "vistas": 407,
+     "vistas": 408,
      "likes": 7,
      "comentarios": 0
     },
@@ -218,7 +218,7 @@ window.YOUTUBE_DATA = {
      "fecha": "2026-03-15T23:00:06Z",
      "miniatura": "https://i.ytimg.com/vi/aNONVOmvrOY/mqdefault.jpg",
      "url": "https://www.youtube.com/watch?v=aNONVOmvrOY",
-     "vistas": 494,
+     "vistas": 496,
      "likes": 8,
      "comentarios": 4
     },
@@ -268,7 +268,7 @@ window.YOUTUBE_DATA = {
      "fecha": "2026-02-23T23:00:21Z",
      "miniatura": "https://i.ytimg.com/vi/v6LRCc5K8CU/mqdefault.jpg",
      "url": "https://www.youtube.com/watch?v=v6LRCc5K8CU",
-     "vistas": 275,
+     "vistas": 277,
      "likes": 5,
      "comentarios": 2
     },
