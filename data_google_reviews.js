60bfb9943e55ef811be5ea02d63d076c73df1c95
Irving Fabian <stylemuxe@gmail.com>
2026-09-09 00:17:21 -0600

    Actualizacion automatica de resenas de Google 2026-09-09 00:17

--- data_google_reviews.js
+++ data_google_reviews.js
@@ -1,5 +1,5 @@
 window.GOOGLE_REVIEWS_DATA = {
- "actualizado": "2026-09-08T23:06:14",
+ "actualizado": "2026-09-09T00:17:21",
  "sucursales": [
   {
    "nombre": "Balbuena",
@@ -285,6 +285,11 @@ window.GOOGLE_REVIEWS_DATA = {
   },
   {
    "fecha": "2026-09-08",
+   "total_resenas": 0,
+   "rating_prom": null
+  },
+  {
+   "fecha": "2026-09-09",
    "total_resenas": 0,
    "rating_prom": null
   }
